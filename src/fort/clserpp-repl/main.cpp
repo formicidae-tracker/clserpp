a81ebe5400a8f3b562ec1af9134cede006415273
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-11-20 15:40:42 +0100

    Implements a basic repl loop

--- src/fort/clserpp-repl/main.cpp
+++ src/fort/clserpp-repl/main.cpp
@@ -1,6 +1,7 @@
 #include <cpptrace/utils.hpp>
 #include <fort/clserpp/clserpp.hpp>
 #include <iostream>
+#include <string>
 using namespace fort::clserpp;
 
 void execute(int argc, char **argv) {
@@ -11,6 +12,23 @@ void execute(int argc, char **argv) {
 	}
 	for (const auto &desc : descriptions) {
 		std::cout << desc.index << ": " << desc.info << std::endl;
+	}
+
+	std::cout << "Please choose an interface:" << std::endl;
+	std::string line;
+	std::getline(std::cin, line);
+	auto serial = Serial::Open(std::stoi(line));
+
+	while (true) {
+		std::string res;
+		line.clear();
+		std::cout << "<<< " << std::endl;
+		if (!std::getline(std::cin, line)) {
+			break;
+		}
+		serial->WriteAll(line, 1000);
+		serial->ReadLine(res);
+		std::cout << ">>> " << res << std::endl;
 	}
 }
 
