2dd3715b8358eac4d41b357c8a992e00ba21a3cb
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-11-21 10:44:56 +0100

    Uses magic_enum

--- src/fort/clserpp/clserpp.hpp
+++ src/fort/clserpp/clserpp.hpp
@@ -1,14 +1,18 @@
 #pragma once
 
-#include "clser.h"
-#include <cpptrace/exceptions.hpp>
 #include <functional>
+#include <iomanip>
+#include <iostream>
 #include <memory>
 #include <vector>
 
+#include <cpptrace/exceptions.hpp>
+
+#include <magic_enum/magic_enum.hpp>
+
+#include "clser.h"
+
 #include "details.hpp"
-#include <iomanip>
-#include <iostream>
 
 namespace fort {
 namespace clserpp {
@@ -48,186 +52,149 @@ public:
 	}
 };
 
-    class Serial {
+class Serial {
 
-	public:
-		static std::unique_ptr<Serial> Open(uint32_t idx) {
-			auto *res = new Serial(idx);
-			return std::unique_ptr<Serial>(res);
-		}
+public:
+	static std::unique_ptr<Serial> Open(uint32_t idx) {
+		auto *res = new Serial(idx);
+		return std::unique_ptr<Serial>(res);
+	}
 
-		static uint32_t NumSerial() {
-			uint32_t res = 0;
-			details::call(clGetNumSerialPorts, &res);
-			return res;
-		}
+	static uint32_t NumSerial() {
+		uint32_t res = 0;
+		details::call(clGetNumSerialPorts, &res);
+		return res;
+	}
 
-		static std::vector<SerialDescription> GetDescriptions() {
-			const auto size = NumSerial();
+	static std::vector<SerialDescription> GetDescriptions() {
+		const auto size = NumSerial();
 
-			std::vector<SerialDescription> res(size);
-			for (uint32_t i = 0; i < size; i++) {
-				char     buffer[DefaultBufferSize];
-				uint32_t size = DefaultBufferSize;
-				details::call(clGetSerialPortIdentifier, i, buffer, &size);
-				res[i] = {.index = i, .info = buffer};
-			}
-			return res;
-		}
-
-		static ManufacturerInfo GetManufacturerInfos() {
+		std::vector<SerialDescription> res(size);
+		for (uint32_t i = 0; i < size; i++) {
 			char     buffer[DefaultBufferSize];
 			uint32_t size = DefaultBufferSize;
-			uint32_t version;
-			details::call(clGetManufacturerInfo, buffer, &size, &version);
-			return {
-			    .name    = buffer,
-			    .version = details::version_name(clVersion_e(version))};
+			details::call(clGetSerialPortIdentifier, i, buffer, &size);
+			res[i] = {.index = i, .info = buffer};
 		}
+		return res;
+	}
 
-		~Serial() {
-			clSerialClose(d_serial);
-		}
+	static ManufacturerInfo GetManufacturerInfos() {
+		char     buffer[DefaultBufferSize];
+		uint32_t size = DefaultBufferSize;
+		uint32_t version;
+		details::call(clGetManufacturerInfo, buffer, &size, &version);
+		return {
+		    .name    = buffer,
+		    .version = std::string{magic_enum::enum_name(clVersion_e(version))},
+		};
+	}
 
-		void Flush() const {
-			details::call(clFlushPort, d_serial);
-		}
+	~Serial() {
+		clSerialClose(d_serial);
+	}
 
-		template <typename Container>
-		void ReadAll(Container &buf, uint32_t timeout_ms) {
-			uint32_t read = 0;
-			while (read < buf.size()) {
-				uint32_t size = buf.size() - read;
-				try {
-					details::call(
-					    clSerialRead,
-					    d_serial,
-					    &buf[read],
-					    &size,
-					    timeout_ms
-					);
-				} catch (const details::clserException &e) {
-					if (e.code() == -10004) {
-						throw IOTimeout(read);
-					}
-					throw;
-				}
-				if (size == 0) {
+	void Flush() const {
+		details::call(clFlushPort, d_serial);
+	}
+
+	template <typename Container>
+	void Read(Container &buf, uint32_t timeout_ms) {
+		uint32_t read = 0;
+		while (read < buf.size()) {
+			uint32_t size = buf.size() - read;
+			try {
+				details::call(
+				    clSerialRead,
+				    d_serial,
+				    &buf[read],
+				    &size,
+				    timeout_ms
+				);
+			} catch (const details::clserException &e) {
+				if (e.code() == -10004) {
 					throw IOTimeout(read);
 				}
-				read += size;
+				throw;
 			}
+			if (size == 0) {
+				throw IOTimeout(read);
+			}
+			read += size;
 		}
+	}
 
-		template <typename Container>
-		void WriteAll(const Container &buf, uint32_t timeout_ms) {
-			uint32_t written = 0;
-			while (written < buf.size()) {
-				uint32_t size = buf.size() - written;
-				try {
-					details::call(
-					    clSerialWrite,
-					    d_serial,
-					    &buf[written],
-					    &size,
-					    timeout_ms
-					);
-				} catch (const details::clserException &e) {
-					if (e.code() == -10004) {
-						throw IOTimeout(written);
-					}
-					throw;
-				}
-
-				if (size == 0) {
+	template <typename Container>
+	void Write(const Container &buf, uint32_t timeout_ms) {
+		uint32_t written = 0;
+		while (written < buf.size()) {
+			uint32_t size = buf.size() - written;
+			try {
+				details::call(
+				    clSerialWrite,
+				    d_serial,
+				    &buf[written],
+				    &size,
+				    timeout_ms
+				);
+			} catch (const details::clserException &e) {
+				if (e.code() == -10004) {
 					throw IOTimeout(written);
 				}
-				written += size;
+				throw;
+			}
+
+			if (size == 0) {
+				throw IOTimeout(written);
+			}
+			written += size;
+		}
+	}
+
+	uint32_t ByteAvailable() const {
+		uint32_t res = 0;
+		details::call(clGetNumBytesAvail, d_serial, &res);
+		return res;
+	}
+
+	std::vector<clBaudrate_e> SupportedBaudrates() const {
+		uint32_t baudrates = 0;
+		details::call(clGetSupportedBaudRates, d_serial, &baudrates);
+
+		std::vector<clBaudrate_e> res;
+		res.reserve(32);
+		for (int i = 0; i < 32; i++) {
+			clBaudrate_e bd = clBaudrate_e(1 << i);
+			if ((baudrates & bd) != 0) {
+				res.push_back(bd);
 			}
 		}
+		return res;
+	}
 
-		template <typename Container>
-		size_t ReadLine(
-		    Container &buf, char delim = '\n', uint32_t timeout_ms = 1000
-		) {
-			uint32_t read = 0;
-			while (read < DefaultBufferSize) {
-				uint32_t size = DefaultBufferSize - read;
-				try {
-					details::call(
-					    clSerialRead,
-					    d_serial,
-					    &buf[read],
-					    &size,
-					    timeout_ms
-					);
-				} catch (const details::clserException &e) {
-					if (e.code() == -10004) {
-						throw IOTimeout(read);
-					}
+	void SetBaudrate(clBaudrate_e bd) {
+		details::call(clSetBaudRate, d_serial, bd);
+	}
 
-					throw;
-				}
+private:
+	Serial(uint32_t idx) {
+		details::call(clSerialInit, idx, &d_serial);
+	}
 
-				if (size == 0) {
-					throw IOTimeout(read);
-				}
+	Serial(const Serial &other)            = delete;
+	Serial &operator=(const Serial &other) = delete;
+	Serial(Serial &&other)                 = delete;
+	Serial &operator=(Serial &&other)      = delete;
 
-				read += size;
-				buf.resize(read);
+	void *d_serial = nullptr;
 
-				if (std::find(buf.begin(), buf.begin() + read, delim) !=
-				    buf.begin() + read) {
-					break;
-				}
-			}
-			return read;
-		}
-
-		uint32_t ByteAvailable() const {
-			uint32_t res = 0;
-			details::call(clGetNumBytesAvail, d_serial, &res);
-			return res;
-		}
-
-		std::vector<clBaudrate_e> SupportedBaudrates() const {
-			uint32_t baudrates = 0;
-			details::call(clGetSupportedBaudRates, d_serial, &baudrates);
-
-			std::vector<clBaudrate_e> res;
-			res.reserve(32);
-			for (int i = 0; i < 32; i++) {
-				clBaudrate_e bd = clBaudrate_e(1 << i);
-				if ((baudrates & bd) != 0) {
-					res.push_back(bd);
-				}
-			}
-			return res;
-		}
-
-		void SetBaudrate(clBaudrate_e bd) {
-			details::call(clSetBaudRate, d_serial, bd);
-		}
-
-	private:
-		Serial(uint32_t idx) {
-			details::call(clSerialInit, idx, &d_serial);
-		}
-
-		Serial(const Serial &other)            = delete;
-		Serial &operator=(const Serial &other) = delete;
-		Serial(Serial &&other)                 = delete;
-		Serial &operator=(Serial &&other)      = delete;
-
-		void *d_serial = nullptr;
-
-		const static uint32_t DefaultBufferSize = 300;
+	const static uint32_t DefaultBufferSize = 300;
 };
 } // namespace clserpp
 } // namespace fort
 
 std::ostream &operator<<(std::ostream &out, clBaudrate_e e) {
-	return out << fort::clserpp::details::baudrate_name(e);
+	return out << magic_enum::enum_name(e);
 }
 
 std::ostream &operator<<(std::ostream &out, const fort::clserpp::Buffer &buf) {
--- src/fort/clserpp/details.hpp
+++ src/fort/clserpp/details.hpp
@@ -48,42 +48,6 @@ void call(Fnct &&fnct, Args &&...args) {
 	}
 }
 
-inline const char *version_name(clVersion_e version) {
-	switch (version) {
-	case CL_VERSION_NONE:
-		return "Invalid";
-	case CL_VERSION_1_0:
-		return "CL v1.0";
-	case CL_VERSION_1_1:
-		return "CL v1.1";
-	default:
-		return "<unknown CL version>";
-	}
-}
-
-inline const char *baudrate_name(clBaudrate_e r) {
-	switch (r) {
-	case CL_BAUDRATE_9600:
-		return "CL_BAUDRATE_9600";
-	case CL_BAUDRATE_19200:
-		return "CL_BAUDRATE_19200";
-	case CL_BAUDRATE_38400:
-		return "CL_BAUDRATE_38400";
-	case CL_BAUDRATE_57600:
-		return "CL_BAUDRATE_57600";
-	case CL_BAUDRATE_115200:
-		return "CL_BAUDRATE_115200";
-	case CL_BAUDRATE_230400:
-		return "CL_BAUDRATE_230400";
-	case CL_BAUDRATE_460800:
-		return "CL_BAUDRATE_460800";
-	case CL_BAUDRATE_921600:
-		return "CL_BAUDRATE_921600";
-	default:
-		return "<unknown CL baudrate>";
-	}
-}
-
 } // namespace details
 } // namespace clserpp
 } // namespace fort
